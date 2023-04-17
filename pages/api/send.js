e4ce22f82c48bd5926cb28e9bc54caf5eb33420c
Stas Bazylevych <stas.bazylevych@corva.ai>
2023-04-17 16:09:17 +0300

    fixed cors error

--- pages/api/send.js
+++ pages/api/send.js
@@ -1,15 +1,26 @@
 const sgMail = require('@sendgrid/mail')
+const cors = require('micro-cors')();
 
-export default async function(req, res) {
+const send = cors(async(req, res) =>{
     sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
-    const { body } = req
+    const { to,
+        from,
+        subject,
+        html } = JSON.parse(req.body)
 
     try {
-        await sgMail.send(body)
+        await sgMail.send({
+            to,
+            from,
+            subject,
+            html,
+        })
         res.status(200).send('Message sent successfully.')
     } catch (error) {
         console.error('ERROR', error)
-        res.status(400).send('Message not sent, check email server.')
+        res.status(400).send({reason:'Message not sent, check email server.', error:error?.message})
     }
-}
+})
+
+export default send
